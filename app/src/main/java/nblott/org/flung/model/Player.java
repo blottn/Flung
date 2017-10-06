328c6ad93585d369e9b88d2927c11a3a4d6bdda8
U-DESKTOP-AUMAJUM\Nick <blottn@tcd.ie>
2017-10-06 10:15:34 +0100

    fixed bug where player would slide along the ground, also moved around some methods to increase readability

--- app/src/main/java/nblott/org/flung/model/Player.java
+++ app/src/main/java/nblott/org/flung/model/Player.java
@@ -36,88 +36,6 @@ public class Player implements Mobile {
 
 
 
-    public void physTick(Canvas canvas) {
-        adv(canvas);
-        ground(canvas);
-    }
-
-    private void adv(Canvas canvas) {
-        for (Block block : parent.blockList) {
-            if (block.isInPath(this, canvas)) {
-                block.onCollided(this,canvas);
-            }
-        }
-        if (floating(canvas)) {
-            vel.setY(vel.getY() - 2);
-        }
-        top += vel.getY();
-        bottom += vel.getY();
-        right += vel.getX();
-        left += vel.getX();
-    }
-
-    private boolean floating(Canvas canvas) {
-        for (Block block : parent.blockList) {
-            if (this.bottom == canvas.getHeight() - block.getRect().top) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void collideY() {
-        if (Math.abs(vel.getY()) < VEL_CUT_OFF) {
-            vel.setY(0);
-        }
-        else {
-            vel.setY((-vel.getY()) * BOUNCE_FACTOR_Y);
-        }
-    }
-
-    public void collideX() {
-        if (Math.abs(vel.getX()) < VEL_CUT_OFF) {
-            vel.setX(0);
-        }
-        else {
-            vel.setX((-vel.getX()) * BOUNCE_FACTOR_X);
-        }
-    }
-
-
-    private void ground(Canvas canvas) {
-
-        // check is on ground
-        if (bottom < 0) {
-            if (vel.getX() > 0) {
-                vel.setX(vel.getX() - 1);
-            }
-            // below ground
-            vel.setY(-vel.getY());
-            vel.setY(vel.getY()/2);
-            if (vel.getY() < 0.5 && vel.getY() > -0.5) {
-                vel.setY(0);
-            }
-            // move to ground
-            top = height;
-            bottom = 0;
-        } else if (bottom > 0) {
-            //needs a check to ensure not above anything
-            vel.setY(vel.getY() - 2);
-            if (vel.getY() < 0.5 && vel.getY() > -0.5) {
-                vel.setY(0);
-            }
-        } else {// y == 0 ie is on ground
-            if (Math.abs(vel.getX()) > 0) {
-                vel.setX(vel.getX() * BOUNCE_FACTOR_X);
-            }
-        }
-    }
-
-    public void draw(Canvas canvas) {
-        drawable.setBounds(left, canvas.getHeight() - top, right, canvas.getHeight() - bottom);
-        drawable.draw(canvas);
-    }
-
     @Override
     public Rect getFuture() {
         return null;
@@ -156,8 +74,103 @@ public class Player implements Mobile {
 
     @Override
     public void translateY(int offset) {
-
         this.top += offset;
         this.bottom += offset;
+    }
+
+    public void physTick(Canvas canvas) {
+        adv(canvas);
+        move();
+        ground(canvas);
+    }
+
+    private void adv(Canvas canvas) {
+        for (Block block : parent.blockList) {
+            if (block.isInPath(this, canvas)) {
+                block.onCollided(this,canvas);
+            }
+        }
+        if (floating(canvas)) {
+            vel.setY(vel.getY() - 2);
+        }
+        else {
+            vel.setX(vel.getX() * BOUNCE_FACTOR_X);
+            if (Math.abs(vel.getX()) < VEL_CUT_OFF) {
+                vel.setX(0);
+            }
+        }
+    }
+
+    private boolean floating(Canvas canvas) {
+        for (Block block : parent.blockList) {
+            if (this.bottom == canvas.getHeight() - block.getRect().top) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void collideY() {
+        if (Math.abs(vel.getY()) < VEL_CUT_OFF) {
+            vel.setY(0);
+        }
+        else {
+            vel.setY((-vel.getY()) * BOUNCE_FACTOR_Y);
+        }
+    }
+
+    public void collideX() {
+        if (Math.abs(vel.getX()) < VEL_CUT_OFF) {
+            vel.setX(0);
+        }
+        else {
+            vel.setX((-vel.getX()) * BOUNCE_FACTOR_X);
+        }
+    }
+
+
+    private void ground(Canvas canvas) {
+
+        // check is on ground
+        if (bottom < 0) {
+            if (vel.getX() > 0) {
+                vel.setX(vel.getX() * BOUNCE_FACTOR_X);
+                if (Math.abs(vel.getX()) < VEL_CUT_OFF) {
+                    vel.setX(0);
+                }
+            }
+            // below ground
+            vel.setY(-vel.getY());
+            vel.setY(vel.getY()/2);
+            if (vel.getY() < 0.5 && vel.getY() > -0.5) {
+                vel.setY(0);
+            }
+            // move to ground
+            top = height;
+            bottom = 0;
+        } else if (bottom > 0) {
+            //needs a check to ensure not above anything
+            vel.setY(vel.getY() - 2);
+            if (vel.getY() < 0.5 && vel.getY() > -0.5) {
+                vel.setY(0);
+            }
+        } else {// y == 0 ie is on ground
+            if (Math.abs(vel.getX()) > 0) {
+                vel.setX(vel.getX() * BOUNCE_FACTOR_X);
+            }
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        drawable.setBounds(left, canvas.getHeight() - top, right, canvas.getHeight() - bottom);
+        drawable.draw(canvas);
+    }
+
+
+    private void move() {
+        top += vel.getY();
+        bottom += vel.getY();
+        right += vel.getX();
+        left += vel.getX();
     }
 }
