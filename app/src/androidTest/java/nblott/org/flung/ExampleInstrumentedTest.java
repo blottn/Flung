a3c0e43d52a400dadd2782e9124e45a7de4b3fa5
Nicholas Blott <blottn@tcd.ie>
2017-10-06 10:19:01 +0100

    Fixed broken test

--- app/src/androidTest/java/nblott/org/flung/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/nblott/org/flung/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("nblott.org.scrolling", appContext.getPackageName());
+        assertEquals("nblott.org.flung", appContext.getPackageName());
     }
 }
